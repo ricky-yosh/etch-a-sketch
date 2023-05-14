2e2981c60c1460bd6187c6051253b5cb8878d612
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-05-14 11:16:11 -0700

    Add divs to canvas body

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("hello world!");
+document.addEventListener('DOMContentLoaded', function()  {
+    let canvas = document.querySelector("#etchCanvas");
+    const thing = document.createElement('div');
+    
+    for(let row = 0; row < 16; row++) {
+        const row_div = document.createElement('div');
+        row_div.style.display = 'flex';
+        for(let column = 0; column < 16; column++) {
+            const box = document.createElement('div');
+            box.style.width = '50px';
+            box.style.height = '50px';
+            box.style.border = '1px solid black';
+            row_div.appendChild(box);
+        }
+        canvas.appendChild(row_div);
+    }    
+});
