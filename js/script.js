ca1adc02b27511b218e537d01fac3339e9e8a3e2
ricky-yosh <63967513+ricky-yosh@users.noreply.github.com>
2023-05-16 14:04:11 -0700

    Create slider functionality with click & hold

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
 
 const canvasCreator = (size) => {
-    const container = document.querySelector('#etchCanvas')
     // canvas creation
     let canvas = document.querySelector("#etchCanvas");
     const thing = document.createElement('div');
-    containerWidth = container.width;
-    faceSize = containerWidth / size;
+    const containerWidth = 960;
+    let faceSize = containerWidth / size;
     
     for(let row = 0; row < size; row++) {
         const row_div = document.createElement('div');
         row_div.style.display = 'flex';
         row_div.style.width = 'auto';
         row_div.style.height = 'auto';
-        for(let column = 0; column < 16; column++) {
+        for(let column = 0; column < size; column++) {
             const box = document.createElement('div');
             box.style.width = faceSize + 'px';
             box.style.height = faceSize + 'px';
+            box.style.boxSizing = 'border-box';
             box.style.border = '1px dashed grey';
             box.className = 'box';
+            
+            box.addEventListener('mousedown', function() {
+                isDrawing = true;
+                box.style.backgroundColor = 'black';
+            });
+            box.addEventListener('mouseup', function() {
+                isDrawing = false;
+            });
+            box.addEventListener('mouseover', () => {
+                if (isDrawing) {
+                    box.style.backgroundColor = 'black';
+                }
+            });
             row_div.appendChild(box);
         }
         canvas.appendChild(row_div);
@@ -31,28 +44,16 @@ document.addEventListener('DOMContentLoaded', function()  {
     const initialSize = 16;
     canvasCreator(initialSize);
 
-    // box event listeners to draw
-    const boxes = this.querySelectorAll('.box');
-    boxes.forEach((box) => {
-        box.addEventListener('mouseover', () => {
-            if (isDrawing) {
-                box.style.backgroundColor = 'black';
-            }
-        });
-        box.addEventListener('mousedown', () => {
-            isDrawing = true;
-        });
-        box.addEventListener('mouseup', () => {
-            isDrawing = false;
-        })
-    });
-
     // slider functionality
     const slider = document.querySelector('#slider');
     const sliderValue = document.querySelector('#sliderValue');
 
+    // change when slider moved
     slider.addEventListener('input', () => {
         const value = slider.value;
         sliderValue.textContent = value;
+        const container = document.querySelector("#etchCanvas");
+        container.innerHTML = '';
+        canvasCreator(value);
     });
 });
